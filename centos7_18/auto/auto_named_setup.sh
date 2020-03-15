5c6d4245b93a6d6d38008974f1f1d1617abc8a90
m_maru76 <city.mati76@gmail.com>
2020-03-15 18:18:49 +0900

    微修正

--- centos7_18/auto/auto_named_setup.sh
+++ centos7_18/auto/auto_named_setup.sh
@@ -1,15 +1,22 @@
 # Backup
+cp -fv /etc/named.conf /etc/named/backup/named.conf.`date "+%F"`
 cp -fv /var/named/2.168.192.db /var/named/backup/2.168.192.db.`date "+%F"`
 cp -fv /var/named/tabiya.local.lan /var/named/backup/tabiya.local.lan.`date "+%F"`
 
 # Copy
+cp -fv dns_git/study_bind/centos7_18/etc/named.conf /etc/named.conf
 cp -fv dns_git/study_bind/centos7_18/var/named/tabiya.local.lan.zone /var/named/tabiya.local.lan
 cp -fv dns_git/study_bind/centos7_18/var/named/2.168.192.db.zone /var/named/2.168.192.db
 
 # diff
+echo "==================== start config ===================="
+/etc/named.conf /etc/named/backup/named.conf.`date "+%F"`
+echo "==================== end config ===================="
+echo " "
 echo "==================== start tabiya ===================="
 diff -u /var/named/tabiya.local.lan /var/named/backup/tabiya.local.lan.`date "+%F"`
 echo "==================== end tabiya ===================="
+echo " "
 echo "==================== start ip ===================="
 diff -u /var/named/2.168.192.db /var/named/backup/2.168.192.db.`date "+%F"`
 echo "==================== end ip ===================="
